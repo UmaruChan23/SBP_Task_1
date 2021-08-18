6316e9b3a11dc0b2544ac5f72cec197f8ba3db59
UMRchan <f.matorin@mail.ru>
2021-08-18 23:46:27 +0300

    Добавлена запись в файл в несколько потоков

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 fun main() {
     val generator = FileGenerator()
     val reader = Reader()
+    val usage = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()
     val start = System.currentTimeMillis()
     generator.generateFile(170,100)
     reader.read()
     println(System.currentTimeMillis() - start)
+    println(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() - usage)
 }
--- src/main/kotlin/Sorter.kt
+++ src/main/kotlin/Sorter.kt
@@ -1,28 +1,41 @@
 import java.io.BufferedReader
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
 import java.util.*
 import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
+import kotlin.concurrent.thread
+import kotlin.io.path.Path
 
 class Sorter {
     private val cores = Runtime.getRuntime().availableProcessors()
     private val threads = cores * 2 + 1
     val executor = Executors.newFixedThreadPool(threads)
-    private var i = 0
-    private val files = ArrayList<File>()
 
-    fun sort(list: ArrayList<String>){
+    @Volatile
+    private var i: AtomicInteger = AtomicInteger(0)
+
+    @Volatile
+    private var files = ArrayList<File>()
+
+    fun sort(list: ArrayList<String>) {
         val worker = Runnable {
-            val file = File("temp_$i.txt")
-            file.createNewFile()
-            file.writeText("")
-            files.add(file)
-            ++i
-            list.sort()
-            for(str in list) {
-                FileGenerator.append(str, file)
+            var file: File?
+            synchronized(i) {
+                file = File("temp_$i.txt")
+                i.incrementAndGet()
+                files.add(file!!)
             }
+            file?.createNewFile()
+            file?.writeText("")
+            list.sort()
+            for (str in list) {
+                file?.let { FileGenerator.append(str, it) }
+            }
+            file = null
         }
         executor.execute(worker)
     }
@@ -33,38 +46,32 @@ class Sorter {
 
     fun fileSort(finalFile: File) {
         val readers = ArrayList<BufferedReader>()
-        val lines = HashMap<Int, String>()
+        val lines = HashMap<Int, String?>()
         var count = 0
+        finalFile.writeText("")
 
-        for(file in files) {
+        for (file in files) {
             val reader = file.inputStream().bufferedReader()
-            lines.set(count, reader.readLine())
+            lines[count] = reader.readLine()
             readers.add(reader)
             ++count
         }
+        while (lines.size > 0) {
+            val sortedLines = lines.toList()
+                .sortedBy { (_, value) -> value }
+                .toMap()
 
-        lines.toList()
-            .sortedBy { (key, value) -> value }
-            .toMap()
-
-
-        /*
-        while(line1 != null  && line2 != null) {
-            if(line1 > line2) {
-                FileGenerator.append(line2, finalFile)
-                line2 = reader2.readLine()
-            } else if(line1.equals(line2)) {
-                FileGenerator.append(line1, finalFile)
-                FileGenerator.append(line2, finalFile)
-                line1 = reader1.readLine()
-                line2 = reader2.readLine()
-            }
-            else {
-                FileGenerator.append(line1, finalFile)
-                line1 = reader1.readLine()
+            val finalLine = sortedLines.values.toList()[0]
+            val key = sortedLines.keys.toList()[0]
+            val reader = readers[key]
+            finalFile.appendText("$finalLine\n")
+            val temp = reader.readLine()
+            if(temp != null ){
+                lines[key] = temp
+            } else {
+                lines.remove(key)
             }
         }
-
-         */
+        for(file in files) println(Files.delete(Path(file.absolutePath)))
     }
 }
